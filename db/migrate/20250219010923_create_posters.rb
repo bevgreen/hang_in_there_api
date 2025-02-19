f56db67f7b250a6848082d3122f28ae353597838
Chase Phillips <chase.thomas.phillips@gmail.com>
2025-02-19 17:21:42 -0500

    added appropriate files and methods for fetch all posters steps

--- db/migrate/20250219010923_create_posters.rb
+++ db/migrate/20250219010923_create_posters.rb
@@ -1,17 +1,13 @@
 class CreatePosters < ActiveRecord::Migration[7.1]
   def change
     create_table :posters do |t|
-      t.integer :id
       t.string :name
       t.string :description
       t.float :price
       t.integer :year
       t.boolean :vintage
       t.string :img_url
-      t.timestamp :created_at
-      t.timestamp :updated_at
-
-      t.timestamps
+      
     end
   end
 end

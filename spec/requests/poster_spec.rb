8221119845c2919d3da5159994f94c8a5514c3bc
Chase Phillips <chase.thomas.phillips@gmail.com>
2025-02-21 20:19:17 -0500

    added functional testing for show, create, update, and destroy methods in poster spec

--- spec/requests/poster_spec.rb
+++ spec/requests/poster_spec.rb
@@ -59,5 +59,111 @@ RSpec.describe Poster, type: :request do
             end
             
         end
+
+        it "can get one poster by the id #show" do
+           id = Poster.create!(name: "DISASTER", 
+            description: "It's too late to start now.", 
+            price: 35.00, 
+            year: 2023, 
+            vintage: false, 
+            img_url:"https://unsplash.com/photos/brown-brick-building-with-red-car-parked-on-the-side-mMV6Y0ExyIk"
+            ).id
+            get "/api/v1/posters/#{id}"
+            poster = JSON.parse(response.body, symbolize_names: true)
+            expect(poster[:data]).to have_key(:id)
+            expect(poster[:data][:id]).to be_a(Integer)
+          
+            expect(poster[:data]).to have_key(:type)
+            expect(poster[:data][:type]).to eq("poster")
+          
+            expect(poster[:data]).to have_key(:attributes)
+            expect(poster[:data][:attributes]).to be_a(Hash)
+          
+            expect(poster[:data][:attributes]).to have_key(:name)
+            expect(poster[:data][:attributes][:name]).to be_a(String)
+          
+            expect(poster[:data][:attributes]).to have_key(:description)
+            expect(poster[:data][:attributes][:description]).to be_a(String)
+          
+            expect(poster[:data][:attributes]).to have_key(:price)
+            expect(poster[:data][:attributes][:price]).to be_a(Float)
+          
+            expect(poster[:data][:attributes]).to have_key(:year)
+            expect(poster[:data][:attributes][:year]).to be_a(Integer)
+          
+            expect(poster[:data][:attributes]).to have_key(:vintage)
+            expect(poster[:data][:attributes][:vintage]).to be_a(TrueClass).or be_a(FalseClass) 
+          
+            expect(poster[:data][:attributes]).to have_key(:img_url)
+            expect(poster[:data][:attributes][:img_url]).to be_a(String)
+          
+
+
+        end
+
+        it "can create a poster #create" do
+        poster_params = {
+            name: "DISASTER", 
+            description: "It's too late to start now.", 
+            price: 35.00, 
+            year: 2023, 
+            vintage: false, 
+            img_url:"https://unsplash.com/photos/brown-brick-building-with-red-car-parked-on-the-side-mMV6Y0ExyIk"
+            }
+            headers = { "CONTENT_TYPE" => "application/json" }
+            post "/api/v1/posters", headers: headers, params: JSON.generate(poster: poster_params)
+            created_poster = Poster.last
+        
+            expect(response).to be_successful
+            expect(created_poster.name).to eq(poster_params[:name])
+            expect(created_poster.description).to eq(poster_params[:description])
+            expect(created_poster.price).to eq(poster_params[:price])
+            expect(created_poster.year).to eq(poster_params[:year])
+            expect(created_poster.vintage).to eq(poster_params[:vintage])
+            expect(created_poster.img_url).to eq(poster_params[:img_url])
+
+        
+
+
+        end
+
+        it "can update an existing poster#update" do
+            id = Poster.create!(name: "DISASTER", 
+                description: "It's too late to start now.", 
+                price: 35.00, 
+                year: 2023, 
+                vintage: false, 
+                img_url:"https://unsplash.com/photos/brown-brick-building-with-red-car-parked-on-the-side-mMV6Y0ExyIk"
+                ).id
+            previous_name = Poster.last.name
+            poster_params = { name: "CATASTROPHE" }
+            headers = {"CONTENT_TYPE" => "application/json"}
+            patch "/api/v1/posters/#{id}", headers: headers, params: JSON.generate({poster: poster_params})
+            poster = Poster.find_by(id: id)
+
+            expect(response).to be_successful
+            expect(poster.name).to_not eq(previous_name)
+            expect(poster.name).to eq("CATASTROPHE")
+
+        end
+
+        it "can destroy a poster#destroy" do
+            poster = Poster.create!(name: "DISASTER", 
+            description: "It's too late to start now.", 
+            price: 35.00, 
+            year: 2023, 
+            vintage: false, 
+            img_url:"https://unsplash.com/photos/brown-brick-building-with-red-car-parked-on-the-side-mMV6Y0ExyIk"
+            )
+
+            expect(Poster.count).to eq(1)
+        
+            delete "/api/v1/posters/#{poster.id}"
+        
+            expect(response).to be_successful
+            expect(Poster.count).to eq(0)
+            expect{ Poster.find(poster.id) }.to raise_error(ActiveRecord::RecordNotFound)
+        end
+
     end
 end
